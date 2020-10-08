9f584b0b4c6020f0ba93896e6d4aa077afaced86
berkayaskin <berkayaskiin@gmail.com>
2020-10-09 00:58:27 +0300

    added a sheet as iframe from qlik sense

--- App.js
+++ App.js
@@ -14,7 +14,9 @@ const App = () => {
 
 	return (
 		<main className="mashup">
-			<div className="mashup__content">Hello from Qlik!</div>
+			<div className="mashup__content">
+				<iframe src="https://berkayaskin/single/?appid=4ca30ed5-3c3b-4753-9f75-56d1cb6072ae&sheet=FaQeFa&opt=currsel&select=clearall"></iframe>
+			</div>
 		</main>
 	);
 };
